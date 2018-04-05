c0ad3d6991b6dd94d0782f77d46e21cfc977c8aa
Anthony Cunningham <oxa547@gmail.com>
2018-04-04 21:52:12 -0500

    4/4/18

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NavComponent } from './nav/nav.component';
+// add Angular Material components here
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+export class CoreModule { }
